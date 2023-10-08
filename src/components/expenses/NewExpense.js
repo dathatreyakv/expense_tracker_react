c0d680bdb94dc19a6993a49cc392c86bc1044dcf
Venkata Dathatreya K <dathatreyakv@gmail.com>
2023-10-08 17:56:13 +0530

    updating to styled components

--- src/components/expenses/NewExpense.js
+++ src/components/expenses/NewExpense.js
@@ -1,6 +1,44 @@
 import { useState } from 'react';
-import '../../assets/styles/expenses/NewExpense.css';
+// import '../../assets/styles/expenses/NewExpense.css';
 import ExpenseForm from './ExpenseForm';
+import {styled} from 'styled-components';
+
+const NewExpanseDiv = styled.div`
+  background-color: #a892ee;
+  padding: 1rem;
+  margin: 2rem auto;
+  width: 50rem;
+  max-width: 95%;
+  border-radius: 12px;
+  text-align: center;
+  box-shadow: 0 1px 8px rgba(0, 0, 0, 0.25);
+
+  & button {
+    font: inherit;
+    cursor: pointer;
+    padding: 1rem 2rem;
+    border: 1px solid #40005d;
+    background-color: #40005d;
+    color: white;
+    border-radius: 12px;
+    margin-right: 1rem;
+  }
+
+  & button:hover, & button:active {
+    background-color: #510674;
+    border-color: #510674;
+  }
+
+  & button.alternative {
+    color: #220131;
+    border-color: transparent;
+    background-color: transparent;
+  }
+  & button.alternative:hover,
+  & button.alternative:active {
+    background-color: #ddb3f8;
+  }
+`;
 
 const NewExpanse = (props) => {
   const [isEditing, setIsEditing] = useState(false);
@@ -15,10 +53,10 @@ const NewExpanse = (props) => {
   const startEditing = ()=> setIsEditing(true)
   const stopEditing = ()=> setIsEditing(false)
 
-  return <div className='new-expense'>
+  return <NewExpanseDiv className='new-expense'>
     {!isEditing && <button onClick={startEditing}>Add New Expense.</button>}
     { isEditing && <ExpenseForm onCancel={stopEditing} onExpenseDataSave={saveExpenseData}/>}
-  </div>
+  </NewExpanseDiv>
 }
 
 export default NewExpanse;
